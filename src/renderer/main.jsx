c400d06eda29423ca7dcbdc0ed3e5d34bcd3e11a
Joe Black <jrb00013wvu@gmail.com>
2026-03-02 12:30:10 -0500

    Refactors renderer entry to Vite template and wraps app in context providers

--- src/renderer/main.jsx
+++ src/renderer/main.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { ThemeProvider } from './context/ThemeContext';
+import { NotificationProvider } from './context/NotificationContext';
+import './services/recentService';
 import './styles/ide.css';
+import './styles/ide-extended.css';
+import './styles/features.css';
 import './styles/missions.css';
 import './styles/components.css';
 
 ReactDOM.createRoot(document.getElementById('app')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <NotificationProvider>
+        <App />
+      </NotificationProvider>
+    </ThemeProvider>
   </React.StrictMode>
 );
 
